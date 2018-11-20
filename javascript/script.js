42b5d81ef5d4ee5c4f0ddb19794235de9ead051f
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-19 19:23:37 -0700

    Adds numerical and range validations.

--- javascript/script.js
+++ javascript/script.js
@@ -4,7 +4,8 @@ const mostRecentGuess = document.querySelector('.mostRecentGuess');
 const correctGuess = document.querySelector('.correctGuess');
 const guessField = document.querySelector('.userGuess');
 const highLow = document.querySelector('.highLow');
-
+const min = 1;
+const max = 100;
 
 guessField.focus();
 
@@ -13,20 +14,38 @@ guessSubmit.addEventListener('click', guessCheck);
 function guessCheck(event) {
   event.preventDefault();
   let userGuess = Number(guessField.value);
-  mostRecentGuess.textContent = 'Your Most Recent Guess: ' + userGuess;
 
-  if (userGuess === randomNumber) {
-    highLow.textContent = '';
-    correctGuess.textContent = "BOOM!";
-    guessSubmit.disabled = true;
-    guessField.disabled = true;
-    resetGame();
-  } else if (userGuess > randomNumber) {
-    highLow.textContent = "That is too high!";
-  } else if (userGuess < randomNumber) {
-    highLow.textContent = "That is too low!";
-  }
+  if (validateGuess(userGuess)) {
+    mostRecentGuess.textContent = 'Your Most Recent Guess: ' + userGuess;
 
-  guessField.value = '';
-  guessField.focus();
+    if (userGuess === randomNumber) {
+      highLow.textContent = '';
+      correctGuess.textContent = "BOOM!";
+      guessSubmit.disabled = true;
+      guessField.disabled = true;
+      resetGame();
+    } else if (userGuess > randomNumber) {
+      highLow.textContent = "That is too high!";
+    } else if (userGuess < randomNumber) {
+      highLow.textContent = "That is too low!";
+    }
+
+    guessField.value = '';
+    guessField.focus();
+  };
 };
+
+const guessError = document.querySelector('.error');
+
+function validateGuess(guess) {
+  guessError.textContent = '';
+  if (isNaN(guess)) {
+    guessError.textContent = "You need to guess a numberical number.";
+    return false;
+  } else if (guess < min || guess > max) {
+    guessError.textContent = 'Your guess should be between ' + min + ' and ' + max;
+    return false;
+  } else {
+    return true;
+  }
+}
---
+++ javascript/validation.js
@@ -0,0 +1,8 @@
+// $(document).ready(function() {
+//   $('.userGuess').on('input', function() {
+//     var input = $(this).val();
+//     if(input < 0 || input > 101) {
+//       $('#errorMessage').html("Must be a number from 1 - 100");
+//     }
+//   })
+// });
