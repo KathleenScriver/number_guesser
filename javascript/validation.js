fa4adcaddd777543f0268ef25bf030011870b83c
Kathleen Yruegas <kathleenecn@gmail.com>
2018-11-24 09:51:27 -0700

    Annotates the jquery file

--- javascript/validation.js
+++ javascript/validation.js
@@ -1,33 +1,35 @@
-// $(document).ready(function() {
-//   $('.userGuess').on('input', function() {
-//     var input = $(this).val();
-//     if(input < 0 || input > 101) {
-//       $('#errorMessage').html("Must be a number from 1 - 100");
-//     }
-//   })
-// });
-
+// jquery file to help with input validation and disabling of buttons/fields
+// when the document is ready loading, this function will initiate
 $(document).ready(function() {
+  // once a user inputs something into the userGuess field, this function will run
   $('.userGuess').on("input", function() {
+    // enables the Clear Guess button since the user has input something in box
     $('.clearGuess').prop('disabled', false);
+    // enables the game Reset button since as soon as the user types something,
+    // the game has started so there is something to reset if desired
     $('.gameReset').prop('disabled', false);
+    // checks userGuess and if it's blank, run the disableReset function
     if ( $('.userGuess').val() === '') {
       disableReset;
     };
   });
-
+// when user clicks on Submit Guess button, the disableReset function is run
   $('.guessSubmit').on('click', disableReset);
-
+// when user clicks on Clear Guess, this function will run
   $('.clearGuess').on('click', function(event) {
+    // run disableReset function
     disableReset;
+    // run clearContents function
     clearContents;
   });
-
+// declares disableReset function
   function disableReset() {
+    // changes the property of the Clear Guess button to disabled.
     $('.clearGuess').prop('disabled', true);
   };
-
+// declares the clearContents function
   function clearContents() {
+    // sets the userGuess html text to an empty string.
     $('.userGuess').html('');
   }
 });
